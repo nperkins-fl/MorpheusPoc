b073dc18b17e8d2d24d1f61df5b760ad77088bc8
Nick Perkins <nperkins@FrontlineEd.com>
2022-09-22 13:45:35 -0400

    Add project files.

---
+++ MorpheusPoc/Models/ViewRequestViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MorpheusPoc.Controllers
+{
+    public class ViewRequestViewModel
+    {
+        public IDictionary<string, string> RequestHeaders { get; set; }
+
+        public IDictionary<string, string> ResponseHeaders { get; set; }
+
+        public IDictionary<string, string> AdditionalData { get; set; } = new Dictionary<string, string>();
+
+        public IDictionary<string, string> ServerVariables { get; set;}
+    }
+}
