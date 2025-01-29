b7786cb85314abe9a92c8ef1f599ecb49652a97d
Nick Perkins <nperkins@FrontlineEd.com>
2025-01-29 10:28:09 -0500

    read signed headers

--- src/MorpheusPoc/Controllers/HomeController.cs
+++ src/MorpheusPoc/Controllers/HomeController.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Configuration;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
 using MorpheusPoc.Models;
 
 namespace MorpheusPoc.Controllers
@@ -38,7 +44,9 @@ namespace MorpheusPoc.Controllers
     }
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private const string SIGNING_KEY = "su2Vj2GoduhHXlFMOvxh7iGXsJ1Iu1zw4NVerAE4elIN830TAsKzsAKtAJL1iV9B";
+
+        public async Task<ActionResult> Index()
         {
             var model = new ViewRequestViewModel
                         {
@@ -47,10 +55,49 @@ namespace MorpheusPoc.Controllers
                             ServerVariables = GetValues(Request.ServerVariables)
                         };
 
+            await ReadSignedHeader(model, "x-fl-userinfo");
+            await ReadSignedHeader(model, "x-fl-impersonator");
+
             ViewBag.RequestUrl = Request.ResolveRequestUrl();
             
             return View(model);
         }
+
+        private static async Task ReadSignedHeader(ViewRequestViewModel model, string headerName)
+        {
+            if (model.RequestHeaders.TryGetValue(headerName, out string headerValue))
+            {
+
+                var handler = new JsonWebTokenHandler();
+                if (handler.CanReadToken(headerValue))
+                {
+                    
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SIGNING_KEY));
+                    
+                    var parameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false, 
+                        ValidateAudience = false,
+                        ValidateLifetime = false,
+                        ValidateIssuerSigningKey = true,
+                        RequireSignedTokens = true,
+                        IssuerSigningKey = key,
+                    };
+                    
+                    var result = await handler.ValidateTokenAsync(headerValue, parameters);
+                    
+                    if(result.IsValid)
+                    {
+                        foreach (KeyValuePair<string, object> claim in result.Claims)
+                        {
+                            model.AdditionalData.Add(headerName + "_" + claim.Key, claim.Value.ToString());
+                        }
+                    }
+                }
+            }
+        }
+
+
         public async Task<ActionResult> MeshCall()
         {
             var meshTarget = ConfigurationManager.AppSettings["MeshTarget"];
